9efebbc499f42a2085fa4519258e0aa78d9e65d8
Roberto La Croce <robertolacroce@live.it>
2021-12-09 10:47:54 +0100

    Initial Commit

---
+++ TheFork restyle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheFork restyle
+//
+//  Created by Roberto La Croce on 09/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheFork restyle/TheFork_restyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFork_restyleApp.swift
+//  TheFork restyle
+//
+//  Created by Roberto La Croce on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TheFork_restyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
